427d880f43cfba3a938df40ac6942b99bac7b361
Festorah <festusoyebami@gmail.com>
2021-02-25 16:32:23 +0100

    Installingdjango-heroku

--- covid_response/settings.py
+++ covid_response/settings.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import os
 import dj_database_url 
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -138,4 +138,4 @@ LOGIN_URL = 'login'
 
 LOGIN_REDIRECT_URL = 'patients-home'
 
-
+django_heroku.settings(locals())
